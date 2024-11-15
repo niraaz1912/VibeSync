75c3d64f739ff8d54bbdd8a187708dbe4e0934a2
arshPadam <padam.arshdeep@gmail.com>
2024-11-14 18:35:06 -0600

    Made an functioning web app using flask

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+from emotion_analyzer import predict_emotion
+from user_identificaiton import recommend
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # A simple HTML form for user input
+
+@app.route('/get_recommendations', methods=['POST'])
+def get_recs():
+    text = request.form['mood']  # Get the user's mood from the form
+    emotion = predict_emotion(text)  # Analyze the emotion
+    tracks = recommend(emotion)
+    return render_template('recommendations.html', recommendations=tracks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
